75f04df702f30c7164235d5cd38a3f6881e7e827
Cameron Chaparro <cvchaparro@gmail.com>
2018-01-25 22:59:10 -0600

    Add some functionality
    Add some initial functionality that we want, including:
    - inserting a heading with title "Tasks"
    - inserting a TODO subheading and some initial boilerplate text
    - scheduling the TODO item for the current day
    - setting a deadline on the TODO item for the next day
    
    Add a function that will build up the filename based on the date (i.e. the base
    directory followed by "YYYY/MM/DD.org") since it's a pretty logical way to keep
    track of DMOs.
    
    Add a base directory - I'll need to make sure it will be a configurable value -
    in which DMO files will be created.
    - need to figure out how to create intermediate directories so that we actually
    do everything necessary for creating the file.

--- dmo-mode.el
+++ dmo-mode.el
@@ -1,7 +1,29 @@
+(require 'org)
+
+(defvar dmo-base-directory "~/"
+  "The base directory in which to find / store your DMO files.
+Must end with a '/' so as to be properly recognized.")
+
+(defun dmo--create-dmo-file ()
+  (concat dmo-base-directory
+          "/"
+          (format-time-string "%Y")
+          "/"
+          (format-time-string "%m")
+          "/"
+          (format-time-string "%d")
+          ".org"))
+
 (defun dmo-create-for-today (number-of-tasks)
   "Create a DMO for the current day with NUMBER-OF-TASKS initial tasks."
   (interactive "p")
-  (message "Number of tasks: %s" number-of-tasks))
+  (with-temp-file (create-file-buffer (dmo--create-dmo-file))
+    (org-insert-heading)
+    (insert "Tasks")
+    (org-insert-todo-subheading nil)
+    (insert "[%] Title goes here...")
+    (org-deadline nil "+1")
+    (org-schedule nil "")))
 
 (defun dmo-create-for-next-work-day (number-of-tasks)
   "Create a DMO for the next work day with NUMBER-OF-TASKS initial tasks."
@@ -10,7 +32,7 @@
 
 ;;;###autoload
 (define-minor-mode dmo-mode
-  "Manage DMOs (Daily Method of Operation) from within Org mode"
+  "Manage DMOs (Daily Methods of Operation) from within Org mode"
   :lighter " dmo"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c t") 'dmo-create-for-today)
